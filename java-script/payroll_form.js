0767020a21a1e2d9aa51bb0e94c62d1dd90cdf17
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-18 01:12:24 -0400

    modified : validating name : .

--- java-script/payroll_form.js
+++ java-script/payroll_form.js
@@ -1,18 +1,18 @@
 //adding eventlisten to name
-window.addEventListener('DOMContentLoaded', (event) => {
-    const name = document.querySelector('#name');
-    const nameError = document.querySelector('.name-error');
-    name.addEventListener('input', function () {
-        if (name.value.length == 0) {
-            nameError.textContent = "";
-            return;
-        }
+//window.addEventListener('DOMContentLoaded', (event) => {
+    //const name = document.querySelector('#name');
+    //const nameError = document.querySelector('.name-error');
+    //name.addEventListener('input', function () {
+        //if (name.value.length == 0) {
+          //  nameError.textContent = "";
+            //return;
+        }//
         try {
             (new EmployeePayrollData()).name = name.value;
             nameError.textContent = "";
         } catch (e) {
             nameError.textContent = e;
-        }
+        }//
     });
 
     const salary = document.querySelector('#salary');
